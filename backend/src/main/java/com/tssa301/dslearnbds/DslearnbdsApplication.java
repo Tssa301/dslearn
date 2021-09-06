fe1b1e2f307f7971a345766d7205bfa0c6af44af
Tiago Ssa <tiago.silva301@gmail.com>
2021-09-06 19:40:24 +0100

    Backend project created

---
+++ backend/src/main/java/com/tssa301/dslearnbds/DslearnbdsApplication.java
@@ -0,0 +1,12 @@
+package com.tssa301.dslearnbds;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DslearnbdsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DslearnbdsApplication.class, args);
+	}
+}
