0a3550c18119b787cf470ebaba3d2361f335504c
Tiago Ssa <tiago.silva301@gmail.com>
2021-09-07 21:50:16 +0100

    Offer

--- backend/src/main/resources/data.sql
+++ backend/src/main/resources/data.sql
@@ -14,3 +14,6 @@ INSERT INTO tb_user_role (user_id, role_id) VALUES (3, 2);
 INSERT INTO tb_user_role (user_id, role_id) VALUES (3, 3);
 
 INSERT INTO tb_course (name, img_Uri, img_Gray_Uri) VALUES ('Bootcamp HTML', 'https://brandoncarson.com/wp-content/uploads/2019/05/html-course.jpg', 'https://legal123.com.au/wp-content/uploads/2019/01/shopping-support-online.svg');
+
+INSERT INTO tb_offer (edition, start_Moment, end_Moment, course_id) VALUES ('1.0', TIMESTAMP WITH TIME ZONE '2020-11-20T00:00:00Z', TIMESTAMP WITH TIME ZONE '2021-11-20T00:00:00Z', 1);
+INSERT INTO tb_offer (edition, start_Moment, end_Moment, course_id) VALUES ('2.0', TIMESTAMP WITH TIME ZONE '2020-12-20T00:00:00Z', TIMESTAMP WITH TIME ZONE '2021-12-20T00:00:00Z', 1);
