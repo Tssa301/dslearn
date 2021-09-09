62d34c1f62da0a100e4a3d550d6c33ceebe0d92b
Tiago Ssa <tiago.silva301@gmail.com>
2021-09-09 20:02:23 +0100

    Lessons seed, fix: lessonsDone

--- backend/src/main/java/com/tssa301/dslearnbds/entities/Enrollment.java
+++ backend/src/main/java/com/tssa301/dslearnbds/entities/Enrollment.java
@@ -1,10 +1,13 @@
 package com.tssa301.dslearnbds.entities;
 
 import java.time.Instant;
+import java.util.HashSet;
+import java.util.Set;
 
 import javax.persistence.Column;
 import javax.persistence.EmbeddedId;
 import javax.persistence.Entity;
+import javax.persistence.ManyToMany;
 import javax.persistence.Table;
 
 import com.tssa301.dslearnbds.entities.pk.EnrollmentPk;
@@ -23,6 +26,9 @@ public class Enrollment {
 	private Instant refundMoment;
 	private boolean available;
 	private boolean onlyUpdate;
+	
+	@ManyToMany(mappedBy = "enrollmentsDone")
+	private Set<Lesson> lessonsDone = new HashSet<>();
 	
 	public Enrollment() {
 	}
--- backend/src/main/resources/data.sql
+++ backend/src/main/resources/data.sql
@@ -29,5 +29,20 @@ INSERT INTO tb_section (title, description, position, img_Uri, resource_id, prer
 INSERT INTO tb_enrollment (user_id, offer_id, enroll_Moment, refund_Moment, available, only_Update) VALUES (1, 1, TIMESTAMP WITH TIME ZONE '2020-11-20T10:00:00Z', null, true, false);
 INSERT INTO tb_enrollment (user_id, offer_id, enroll_Moment, refund_Moment, available, only_Update) VALUES (2, 1, TIMESTAMP WITH TIME ZONE '2020-11-20T10:00:00Z', null, true, false);
 
+INSERT INTO tb_lesson (title, position, section_id) VALUES ('Lesson 1 of chapter 1', 1, 1);
+INSERT INTO tb_content (id, text_Content, video_Uri) VALUES (1, 'Support material: Lessons', 'https://www.youtube.com/watch?v=tKKWBcYW19I');
+
+INSERT INTO tb_lesson (title, position, section_id) VALUES ('Lesson 2 of chapter 1', 2, 1);
+INSERT INTO tb_content (id, text_Content, video_Uri) VALUES (2, '', 'https://www.youtube.com/watch?v=tKKWBcYW19I');
+
+INSERT INTO tb_lesson (title, position, section_id) VALUES ('Lesson 3 of chapter 1', 3, 1);
+INSERT INTO tb_content (id, text_Content, video_Uri) VALUES (3, '', 'https://www.youtube.com/watch?v=tKKWBcYW19I');
+
+INSERT INTO tb_lesson (title, position, section_id) VALUES ('Chapter 1 Challenge', 4, 1);
+INSERT INTO tb_task (id, description, question_Count, approval_Count, weight, due_Date) VALUES (4, 'Deliver a cool challenge', 5, 4, 1.0, TIMESTAMP WITH TIME ZONE '2020-11-25T10:00:00Z');
+
+INSERT INTO tb_lessons_done (lesson_id, user_id, offer_id) VALUES (1, 1, 1);
+INSERT INTO tb_lessons_done (lesson_id, user_id, offer_id) VALUES (2, 1, 1);
+
 
 
